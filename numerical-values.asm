fd1391118e6f9e8a949aeb27434f5b14b8152fd2
Kokila Abhayasundara <94110759+kvabhaya@users.noreply.github.com>
2023-08-12 15:00:30 +0530

    Create numerical-values.asm

---
+++ numerical-values.asm
@@ -0,0 +1,13 @@
+.model small
+.data
+var1 db 12 ; -128 <= db <= 255
+var2 dw 15 ; -32768 <= dw <= 65535
+var3 db 10101111b
+var4 dw 1101111011101101b
+var5 db 0f2h
+var6 dw 12f2h
+.code
+main proc
+
+ main endp
+end
